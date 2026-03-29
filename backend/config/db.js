e65dca6e8aea48a3d4b710a7877e62f303454582
Vishnu Anup <vishnuanup06@gmail.com>
2026-03-30 01:09:05 +0530

    changed to mongo db atlas from local database

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
 
-    await mongoose.connect("mongodb://127.0.0.1:27017/pixelwit");
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected");
 
